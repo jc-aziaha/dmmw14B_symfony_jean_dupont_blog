22c4a9b276a8c34cd7f5a02da4053d1274efd7ba
Jean Claude <aziaha.formations@gmail.com>
2023-08-03 16:46:44 +0200

    Mise en place de l'espace admin

---
+++ src/Controller/Admin/Home/DashboardController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller\Admin\Home;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractController
+{
+    #[Route('/admin/home/dashboard', name: 'admin.home.dashboard')]
+    public function index(): Response
+    {
+        return $this->render('pages/admin/home/index.html.twig');
+    }
+}
