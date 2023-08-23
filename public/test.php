b2955ed00a6f9241cd68032be90f8b61e1b206be
Jean Claude <aziaha.formations@gmail.com>
2023-08-23 16:41:24 +0200

    Mise en place du système de commentaire

--- public/test.php
+++ public/test.php
@@ -1,34 +1,11 @@
-<?php
-    if ( $_SERVER['REQUEST_METHOD'] === "POST" ) 
-    {
-        var_dump($_FILES);
-        // if (  ) 
-        // {
-        //     # code...
-        // }
-
-        $extension = pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
-
-        var_dump($extension);
-
-        if ( $extension !== "jpg" && $extension !== "jpeg" && $extension !== "png" && $extension !== "webp"  ) 
-        {
-            $errors['image'] = "Le format n'est pas valide.";
-        }
-    }
-?>
-
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        <form method="post" enctype="multipart/form-data">
-            <input type="file" name="image">
-            <input type="submit">
-        </form>
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<iframe width="100%" height="500px" style="box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); border-radius:15px;" allowtransparency="true" allowfullscreen="true" scrolling="no" title="Embedded DrawSQL IFrame" frameborder="0" src="https://drawsql.app/teams/lyoko/diagrams/jean-dupont/embed"></iframe>
+</body>
 </html>
