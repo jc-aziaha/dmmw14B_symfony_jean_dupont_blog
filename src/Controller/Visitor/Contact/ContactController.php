1c4a502aa4fcadf32f5e5c1eedeba72caf150042
Jean Claude <aziaha.formations@gmail.com>
2023-08-24 12:43:24 +0200

    Mise en place du formulaire de contact et envoi d'email

--- src/Controller/Visitor/Contact/ContactController.php
+++ src/Controller/Visitor/Contact/ContactController.php
@@ -2,15 +2,58 @@
 
 namespace App\Controller\Visitor\Contact;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Contact;
+use App\Form\ContactFormType;
+use App\Service\SendEmailService;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ContactController extends AbstractController
 {
-    #[Route('/contact', name: 'visitor.contact.create')]
-    public function create(): Response
+    #[Route('/contact', name: 'visitor.contact.create', methods: ['GET', 'POST'])]
+    public function create(
+        Request $request,
+        EntityManagerInterface $em,
+        SendEmailService $sendEmailService
+    ): Response
     {
-        return $this->render('pages/visitor/contact/create.html.twig');
+        $contact = new Contact();
+
+        $form = $this->createForm(ContactFormType::class, $contact);
+
+        $form->handleRequest($request);
+
+        if ( $form->isSubmitted() && $form->isValid() ) 
+        {
+            $em->persist($contact);
+            $em->flush();
+
+            // Envoi de l'email
+            $sendEmailService->send([
+                "sender_email" => "medecine-du-monde@gmail.com",
+                "sender_name"  => "Jean Dupont",
+                "recipient_email" => "medecine-du-monde@gmail.com",
+                "subject" => "Un message reçu sur votre blog",
+                "html_template" => "email/contact.html.twig",
+                "context"   => [
+                    "contact_first_name"    => $contact->getFirstName(),
+                    "contact_last_name"     => $contact->getLastName(),
+                    "contact_email"         => $contact->getEmail(),
+                    "contact_phone"         => $contact->getPhone(),
+                    "contact_message"       => $contact->getMessage(),
+                ]
+            ]);
+
+            $this->addFlash("success", "Votre message a bien été envoyé. Je vous répondrai dans les plus brefs délais.");
+
+            return $this->redirectToRoute('visitor.contact.create');
+        }
+
+        return $this->render('pages/visitor/contact/create.html.twig', [
+            "form" => $form->createView()
+        ]);
     }
 }
